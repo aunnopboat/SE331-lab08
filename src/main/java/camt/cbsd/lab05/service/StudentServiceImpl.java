15ec86dc9f11a418f2dc22c8b2454a7d58937a4c
AUNNOP\NEOUSER <aunnop_sirisit@hotmail.com>
2017-10-27 01:50:22 +0700

    3.7

--- src/main/java/camt/cbsd/lab05/service/StudentServiceImpl.java
+++ src/main/java/camt/cbsd/lab05/service/StudentServiceImpl.java
@@ -11,7 +11,7 @@ import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
-@Profile("firstDataSource")
+@Profile("DBDatasource")
 @ConfigurationProperties(prefix = "server")
 @Service
 public class StudentServiceImpl implements StudentService {
